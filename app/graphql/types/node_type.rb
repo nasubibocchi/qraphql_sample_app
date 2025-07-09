5c46ccb702d7dd34d9655c341873626ecc8868d5
nasubibocchi <matsuhito125@gmail.com>
2025-07-09 18:21:42 +0900

    役割についてのメモコメントを追加

--- app/graphql/types/node_type.rb
+++ app/graphql/types/node_type.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# --relayオプション（デフォルトで有効）で作成される
+# 
+# 役割:
+#   - 全てのオブジェクトに共通のidフィールドを提供
+#   - GraphQL RelayのGlobal Object Identificationを実現
+#   - 異なるタイプのオブジェクトを統一的に扱う
+
+#   使用例:
+#   class UserType < Types::BaseObject
+#     implements GraphQL::Types::Relay::Node  # NodeTypeを実装
+
+#     field :id, ID, null: false  # 自動的に提供される
+#     field :name, String, null: false
+#   end
+
 module Types
   module NodeType
     include Types::BaseInterface
