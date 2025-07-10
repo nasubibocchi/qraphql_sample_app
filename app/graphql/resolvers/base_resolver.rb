e3b24d05ff5c7d4d9951db2e8e6d442633bc2904
nasubibocchi <matsuhito125@gmail.com>
2025-07-10 09:24:43 +0900

    resolverの説明を追加

--- app/graphql/resolvers/base_resolver.rb
+++ app/graphql/resolvers/base_resolver.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# このクラスをリゾルバートして使う場合は、各クエリで指定する
+# 例：
+# field :user, resolver: Resolvers::UserResolver (UserResolverはBaseResolverを継承)
+# Mutationクラスを参考にするとわかりやすい
+
 module Resolvers
   class BaseResolver < GraphQL::Schema::Resolver
   end
